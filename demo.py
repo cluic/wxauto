2e0a011918df4bb9c441fe17fb07e4e0b50f87c9
Moenea <1430293748@qq.com>
2024-05-04 15:03:28 +0800

    master

--- demo.py
+++ demo.py
@@ -5,12 +5,12 @@ wx = WeChat()
 # 发送消息
 who = '文件传输助手'
 for i in range(3):
-    wx.SendMsg(f'wxauto测试{i+1}', who)
+    wx.SendMsg(f'wxauto测试{i}', who)
     
 # 获取当前聊天页面（文件传输助手）消息，并自动保存聊天图片
-msgs = wx.GetAllMessage(savepic=True)
+# msgs = wx.GetAllMessage(savepic=True)
+msgs = wx.GetAllMessage(savepic=False)
 for msg in msgs:
     print(f"{msg[0]}: {msg[1]}")
-
 
 print('wxauto测试完成！')
